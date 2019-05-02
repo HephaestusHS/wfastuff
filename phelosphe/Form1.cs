67b49f584486a1004144ae6af5b244fc880a5cf1
hachiroku <ichhabeviele@gmail.com>
2019-05-02 23:10:31 +0300

    first commit

---
+++ phelosphe/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace phelosphe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackColor = ColorTranslator.FromHtml("#311ebf");
+            //sidePanel.PaintPanel("#311ebf"); ---> first color
+            sidePanel.PaintPanel("#1c1168");
+            sidePanel.GiveLocationToPanel(this);
+            sidePanel.CreateMoneyLabels();
+            moneyPanel.Location = new Point(0, 960);
+            moneyPanel.Parent = sidePanel;
+            moneyPanel.Width = 300;
+            moneyPanel.Height = 80;
+            moneyPanel.BackColor = ColorTranslator.FromHtml("#1c1168");
+            sidePanel.Controls.SetChildIndex(moneyPanel, 300);
+            AnswerPanelOne.CreateAPanel(this, 0, this.Height - 300, (this.Width - 300) / 2, 110);
+            AnswerPanelTwo.CreateAPanel(this, (this.Width - 300) / 2 - 1, this.Height - 300, (this.Width - 300) / 2, 110);
+            AnswerPanelThree.CreateAPanel(this, 0, this.Height - 160, (this.Width - 300) / 2, 110);
+            AnswerPanelFour.CreateAPanel(this, (this.Width - 300) / 2 - 1, this.Height - 160, (this.Width - 300) / 2, 110);
+            this.Controls.SetChildIndex(AnswerPanelThree , 999);
+            this.Controls.SetChildIndex(AnswerPanelFour, 999);
+            //pnl.CreateQPanel(this, (this.Width - 300) / 2, this.Height - 160, 20, 10);
+            //AnswerPanelThree.BackColor = Color.Black;
+            QuePnl.CreateAPanel(this, 0, this.Height - 545, this.Width - 300, 200);
+            JokerPanelOne.CreateAPanel(this, 30, 21, 90, 75);
+            JokerPanelOne.GiveBgImage(@"C:\Users\hachi\source\repos\phelosphe\phelosphe\Images\book.png", 15, 25, 60, 34);
+            JokerPanelTwo.CreateAPanel(this, 160, 21, 90, 75);
+            JokerPanelThree.CreateAPanel(this, 290, 21, 90, 75);
+            JokerPanelTwo.CreateLabelforPanel("½", 22, 18, 50, 50);
+            JokerPanelThree.GiveBgImage(@"C:\Users\hachi\source\repos\phelosphe\phelosphe\Images\pfp.png", 26, 18, 40, 45);
+            this.WindowState = FormWindowState.Maximized;
+            this.FormBorderStyle = FormBorderStyle.None;
+        }
+    }
+}
---
+++ phelosphe/Methods.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace phelosphe
+{
+    public static class Methods
+    {
+        public static void PaintPanel(this Panel panel, string hexCode)
+        {
+            panel.BackColor = ColorTranslator.FromHtml(hexCode);
+        }
+        public static void GiveLocationToPanel(this Panel panel, Form1 form)
+        {
+            panel.Height = form.Height;
+            panel.Location = new Point(1620, 0);
+            panel.Width = 300;
+        }
+        public static void CreateMoneyLabels(this Panel sidePanel)
+        {
+            List<Label> labels = new List<Label>();
+            string[] moneyArray = new string[] { "1000", "2000", "5000", "10000", "20000", "50000", "100000", "250000", "500000", "1000000" };
+            int LblHeight = 980;
+            for (byte i = 0; i < 10; i++)
+            {
+                Label lbl = new Label()
+                {
+                    Name = "MoneyLabel" + i.ToString(),
+                    ForeColor = ColorTranslator.FromHtml("#fff"),
+                    BackColor = Color.Transparent,
+                    BorderStyle = BorderStyle.None,
+                    Location = new Point(45, LblHeight),
+                    Text = moneyArray[i],
+                    TabIndex = 1,
+                    Font = new Font("segoe ui", 20, FontStyle.Bold),
+                    Height = 50,
+                    Width = 200,
+                    TextAlign = ContentAlignment.TopCenter,
+                    Parent = sidePanel,
+                };
+                lbl.BringToFront();
+                sidePanel.Controls.Add(lbl);
+                sidePanel.Controls.SetChildIndex(lbl, 600);
+                LblHeight -= 105;
+            }
+        }
+        public static void CreateAPanel(this Panel panel, Form1 form, int x, int y, int width, int height)
+        {
+            panel.Height = height;
+            panel.Width = width; 
+            panel.Location = new Point(x, y);
+            panel.BackColor = Color.Transparent;
+            panel.Parent = form;
+        }
+        public static void GiveBgImage(this Panel panel, string imagePath, int x, int y, int width, int height)
+        {
+            Bitmap bgImage = new Bitmap(imagePath);
+            Bitmap newBgImage = new Bitmap(bgImage.Width, bgImage.Height);
+            Graphics graphics = Graphics.FromImage(newBgImage);
+            graphics.DrawImage(bgImage, x, y, width, height);
+            panel.BackgroundImage = newBgImage;
+        }
+        public static void CreateLabelforPanel(this Panel panel, string text, int x, int y, int width, int height)
+        {
+            Label label = new Label();
+            label.Parent = panel;
+            label.Location = new Point(x, y);
+            label.Text = text;
+            label.Height = height;
+            label.Width = width;
+            label.Font = new Font("segoe ui", 25, FontStyle.Bold);
+            label.ForeColor = Color.White;
+            panel.Controls.Add(label);
+        }
+    }
+}
